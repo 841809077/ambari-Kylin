0f499c00d1b81c453847765445a143935e05a05a
841809077 <liuyongzhi95@163.com>
2018-12-13 22:11:45 +0800

    ambari集成Apache Kylin服务脚本

---
+++ package/scripts/kylin_query.py
@@ -0,0 +1,89 @@
+import pwd
+import grp
+from resource_management import *
+
+
+class KylinQuery(Script):
+    def install(self, env):
+        import params
+        env.set_params(params)
+
+        # Create user and group for kylin if they don't exist
+        try:
+            grp.getgrnam(params.kylin_group)
+        except KeyError:
+            Group(group_name=params.kylin_group)
+
+        try:
+            pwd.getpwnam(params.kylin_user)
+        except KeyError:
+            User(username=params.kylin_user,
+                 gid=params.kylin_group,
+                 groups=[params.kylin_group],
+                 ignore_failures=True
+                 )
+        # create kylin directories
+        Directory([params.kylin_install_dir, params.kylin_log_dir, params.kylin_pid_dir],
+                  mode=0755,
+                  cd_access='a',
+                  owner=params.kylin_user,
+                  group=params.kylin_group,
+                  create_parents=True
+                  )
+        # download kylin-5.2.1.tar.gz
+        Execute('wget {0} -O kylin-5.2.1.tar.gz'.format(params.kylin_download))
+        # Install kylin
+        Execute('tar -zxvf kylin-5.2.1.tar.gz -C {0}'.format(params.kylin_install_dir))
+        # Remove kylin installation file
+        Execute('rm -rf kylin-5.2.1.tar.gz')
+        # Ensure all files owned by kylin user:group
+        cmd = format("chown -R {kylin_user}:{kylin_group} {kylin_install_dir}")
+        Execute(cmd)
+
+    def configure(self, env):
+        import params
+        env.set_params(params)
+        kylin_properties = InlineTemplate(params.kylin_properties)
+        File(format("{kylin_install_dir}/conf/kylin.properties"),
+             owner=params.kylin_user,
+             group=params.kylin_group,
+             content=kylin_properties)
+
+        File(format("{tmp_dir}/kylin_init.sh"),
+             content=Template("init.sh.j2"),
+             owner=params.kylin_user,
+             group=params.kylin_group,
+             mode=0o700
+             )
+        # create hadoop_conf_dir
+        cmd = format("sh {tmp_dir}/kylin_init.sh")
+        Execute(cmd, user=params.kylin_user)
+
+    def start(self, env):
+        import params
+        env.set_params(params)
+        self.configure(env)
+        cmd = format(
+            "{kylin_install_dir}/bin/kylin.sh start;cp -rf {kylin_install_dir}/pid {kylin_pid_file}")
+        Execute(cmd, user=params.kylin_user)
+
+    def stop(self, env):
+        import params
+        env.set_params(params)
+        cmd = format("{kylin_install_dir}/bin/kylin.sh stop")
+        File(params.kylin_pid_file,
+             action="delete",
+             owner=params.kylin_user
+             )
+        Execute(cmd, user=params.kylin_user)
+
+    def restart(self, env):
+        self.stop(env)
+        self.start(env)
+
+    def status(self, env):
+        check_process_status('/var/run/kylin/kylin.pid')
+
+
+if __name__ == "__main__":
+    KylinQuery().execute()
---
+++ package/scripts/params.py
@@ -0,0 +1,51 @@
+from resource_management import *
+from resource_management.libraries.script.script import Script
+import os
+
+# server configurations
+config = Script.get_config()
+service_packagedir = os.path.realpath(__file__).split('/scripts')[0]
+tmp_dir = Script.get_tmp_dir()
+
+ambari_server_hostname = config['clusterHostInfo']['ambari_server_host'][0]
+kylin_download = os.path.join('http://', ambari_server_hostname, 'kylin/kylin-5.2.1.tar.gz')
+
+kylin_user = config['configurations']['kylin']['kylin_user']
+kylin_group = config['configurations']['kylin']['kylin_group']
+kylin_install_dir = config['configurations']['kylin']['kylin_install_dir']
+kylin_log_dir = config['configurations']['kylin']['kylin_log_dir']
+kylin_pid_dir = config['configurations']['kylin']['kylin_pid_dir']
+kylin_pid_file = format("{kylin_pid_dir}/kylin.pid")
+
+hdp_version = default("/commandParams/version", None)
+kylin_web_port = config['configurations']['kylin']['kylin_web_port']
+
+kylin_properties = config['configurations']['kylin']['kylin_properties']
+
+# kylin web timezone
+kylin_web_timezone = config['configurations']['kylin']['kylin_web_timezone']
+# kylin_web_cross_domain_enabled
+kylin_web_cross_domain_enabled = config['configurations']['kylin']['kylin_web_cross_domain_enabled']
+if (kylin_web_cross_domain_enabled):
+    kylin_web_cross_domain_enabled = 'true'
+else:
+    kylin_web_cross_domain_enabled = 'false'
+
+current_host_name = config['hostname']
+server_mode = "query"
+server_masters = config['clusterHostInfo']['kylin_all_hosts'][0]
+server_clusters_arr = config['clusterHostInfo']['kylin_all_hosts'] + (
+    config['clusterHostInfo'].has_key('kylin_query_hosts') and config['clusterHostInfo']['kylin_query_hosts'] or [])
+
+server_clusters = ','.join(i + ":" + kylin_web_port for i in server_clusters_arr)
+kylin_servers = ';'.join("server " + i + ":" + kylin_web_port for i in server_clusters_arr) + ";"
+hadoop_conf_dir = kylin_install_dir + "/conf/hadoop_conf"
+
+# ngnix
+nginx_download = os.path.join('http://', ambari_server_hostname, 'kylin/nginx-1.8.1.tar.gz')
+nginx_install_dir = config['configurations']['nginx']['nginx_install_dir']
+nginx_conf = config['configurations']['nginx']['nginx_conf']
+nginx_port = config['configurations']['nginx']['nginx_port']
+nginx_log_dir = config['configurations']['nginx']['nginx_log_dir']
+nginx_pid_dir = config['configurations']['nginx']['nginx_pid_dir']
+nginx_pid_file = format("{nginx_pid_dir}/nginx.pid")
